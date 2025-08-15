fe75d4b1c5b0096262fd3260ca5780bb8d513b62
Ahmad Khalid Safi <khalidsafi031@gmail.com>
2025-08-15 11:18:20 +0430

    This is a new commit

--- temp.js
+++ temp.js
@@ -1,11 +1,1 @@
-let one = "     Hello     ";
-let two = "     Bitch!    ";
-
-console.log(one + two);
-
-
-let three = one + two;
-
-console.log(three.trimStart());
-
-console.log(typeof one);
+let bro = true; 
