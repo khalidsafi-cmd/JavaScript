a36a2d250e818c6016d6a8bfe95c3ace43d7b574
Ahmad Khalid Safi <khalidsafi031@gmail.com>
2025-08-15 12:10:18 +0430

    new practices in javascript

--- condition.js
+++ condition.js
@@ -3,3 +3,15 @@ let temprature = 26;
 const weather = temprature > 25 ? 'Warm' : 'Cool'; 
 
 console.log(`its a really beautiful ${weather} Day`);
+
+
+
+temprature = 24; 
+
+if (temprature > 25) {
+    console.log(`The weather is very warm`);
+}
+else {
+    console.log(`The weather is cool`);
+    
+}
---
+++ math.js
@@ -0,0 +1,5 @@
+const randomNumber = Math.random(); 
+
+
+const randomNumBtw1And20 = Math.floor(Math.random() * 20) + 1;
+console.log(randomNumBtw1And20); 
