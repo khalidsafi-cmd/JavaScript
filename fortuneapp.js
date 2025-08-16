75d3374f5ce67cddb3dd754e1fb642da50bd51bb
Ahmad Khalid Safi <khalidsafi031@gmail.com>
2025-08-16 13:12:59 +0430

    new changes were made to the javascript directory

--- fortuneapp.js
+++ fortuneapp.js
@@ -5,8 +5,9 @@ const botName = "FortuneBot";
 const greeting = `Hello! My name is ${botName} and I am here to share your fortune!`;
 console.log(greeting);
 
-let randomNum = Math.floor(Math.random() * (max - min) + min);
-console.log(`Your random fortune number is: ${Math.floor(randomNum)}`);
+let randomNumber = Math.floor(Math.random() * (max - min +1) + min);
+
+console.log(`Your random fortune number is: ${Math.floor(randomNumber)}`);
 
 console.log("Generating a random fortune number between 1 and 5...");
 
@@ -16,18 +17,23 @@ let fortune3 = "Your hard work will soon pay off.";
 let fortune4 = "An unexpected event will bring you joy.";
 let fortune5 = "You will meet someone special soon.";
 
-if (randomNum == 1) {
-    console.log(fortune1);
+let selectedFortune;
+if (randomNumber == 1){
+  selectedFortune = fortune1;
 }
-else if (randomNum == 2) {
-    console.log(fortune2);
+else if (randomNumber == 2){
+  selectedFortune = fortune2;
 }
-else if (randomNum == 3) {
-    console.log(fortune3);
-}   
-else if (randomNum == 4) {
-    console.log(fortune4);
-}   
+else if (randomNumber == 3){
+  selectedFortune = fortune3;
+}
+else if (randomNumber == 4){
+  selectedFortune = fortune4;
+}
 else {
-    console.log(fortune5);
+  selectedFortune = fortune5;
 }
+
+console.log(selectedFortune);
+
+
--- math.js
+++ math.js
@@ -2,4 +2,11 @@ const randomNumber = Math.random();
 
 
 const randomNumBtw1And20 = Math.floor(Math.random() * 20) + 1;
-console.log(randomNumBtw1And20); 
+console.log(randomNumBtw1And20);
+
+
+
+
+const number = 10.53432;
+
+console.log(number.toFixed(2)); // Outputs: 10.53
